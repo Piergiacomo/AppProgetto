cd065088af19a8e74ff6bcb720d70ac8deea75e8
Piergiacomo <chiarelli.piergiacomo@gmail.com>
2018-02-04 12:20:36 +0100

    Default dell'applicazione..prova della composizione

---
+++ ProgettoFineCorso/ProgettoFineCorso/ProgettoFineCorso/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ProgettoFineCorso.Models;
+
+namespace ProgettoFineCorso.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
